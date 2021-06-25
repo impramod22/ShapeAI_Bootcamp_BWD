6fee76dbc30a7a4699591ad3cc2fa6d8285f5f0a
impramod22 <79921728+impramod22@users.noreply.github.com>
2021-06-25 13:47:47 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript And React.JS Bootcamp</h1>
+      <p>A basic 7 days Web Dev Bootcamp on JavaScript and React.JS </p>
+    </div>
+  );
+}
+export default Info;
